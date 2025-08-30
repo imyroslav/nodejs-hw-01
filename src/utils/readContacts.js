e19b525e5efc43592d593ff295699dc4a369fcf5
imyroslav <imyroslav@ukr.net>
2025-08-30 21:51:01 +0200

    add-read-write-contacts

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,16 @@
-// import { PATH_DB } from '../constants/contacts.js';
+import fs from "node:fs/promises";
+// const path = require("path")
 
-// export const readContacts = async () => {};
+import { PATH_DB } from '../constants/contacts.js';
+
+export const readContacts = async () => {
+    try {
+        const data = await fs.readFile(PATH_DB, "utf-8");
+        const contacts = JSON.parse(data);
+        return contacts;
+
+    } catch (error) {
+        console.error("Error occured during file reading", error.message);
+        
+    }
+};
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,15 @@
-// import { PATH_DB } from '../constants/contacts.js';
+import fs from "node:fs/promises";
 
-// export const writeContacts = async (updatedContacts) => {};
+import { PATH_DB } from '../constants/contacts.js';
+
+export const writeContacts = async (updatedContacts) => {
+    const data = JSON.stringify(updatedContacts, null, 2);
+
+    try {
+        await fs.writeFile(PATH_DB, data, "utf-8");
+        console.log("Data is succesfully written")
+
+    } catch (error) {
+        console.error("Error occured during writing data", error.message)
+    }
+};
